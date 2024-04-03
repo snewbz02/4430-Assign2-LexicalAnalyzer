4d73d96abb609c294711751b15174e830f1da7f5
Sienna Newby-Sanchez <snewbz02@gmail.com>
2024-04-02 23:05:04 -0500

    Add keyword and operator definitions
    Defining keywords and operators using an unordered_set for further processing in the program.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cctype>
+#include <unordered_set>
+
+using namespace std;
+
+// Define keywords and operators
+unordered_set<string> keywords = 
+{
+    
+    "if", "while", "break", "int", "float", "char", "long", "typedef", "unsigned",
+    "static", "sizeof", "volatile", "enum", "union", "bool", "else", "do", "continue",
+    "double", "return", "case", "short", "switch", "void", "struct", "long", "typedef",
+    "const", "extern", "auto", "default", "for"
+    
+};
